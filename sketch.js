6cbcd5bf81a26e72b5262c9785e5debf34fcea6e
Jordan Jones <jordan.jones@mintellity.com>
2023-07-29 12:11:38 -0400

    shapes in the void; add more thoughts

--- sketch.js
+++ sketch.js
@@ -1,37 +1,77 @@
-let deg = 12;
-let value = 0;
+let s = 'I need money';
+let v = 'Can I kiss you?';
+let k = 'Once I do this...';
+let j = 'We have to have sex';
+let d = 'When will I hoop again?';
+
+
 
 function setup() {
-  createCanvas(400, 400);
-  let onlyInSetup =5;
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(0);
-  fill(255,0,0);
-  deg+=3;
-  rotatingCiricle();
-
-}
-
-function rotatingCiricle() {
-  push(); 
-  translate (200,200);
-  rotate (radians (deg));
-  ellipse (0,0,170,170);
-  // line(0, 20, 0, 0);
-  pop(); 
-  rotatingText();
-}
-
-function rotatingText() {
-  
-  push();
   fill(255);
-    translate(170,170);
-    rotate( radians(deg) );
-    text("ROTATE ME", 40, 20);
-	pop();
+  words();
+
 }
+
+
+
+
+
+function words() {
+  push()
+  let word1;
+  fill(255);
+  textAlign(CENTER);
+  translate(width/2, height/2);
+  translate(p5.Vector.fromAngle(millis() / 500, 80));
+  word1 = text(s, width/15, height/15);
+  pop()
+
+  push()
+  let word2;
+  fill(255);
+  textAlign(CENTER);
+  translate(width/2, height/2);
+  translate(p5.Vector.fromAngle(millis() / 700, 80));
+  word2 = text(v, width/40, height/40);
+  pop()
+
+  push()
+  let word3;
+  fill(255);
+  textAlign(CENTER);
+  translate(width/2, height/2);
+  translate(p5.Vector.fromAngle(millis() / 400, 80));
+  word3 = text(k, width/10, height/10);
+  pop()
+
+  push()
+  let word4;
+  fill(255);
+  textAlign(CENTER);
+  translate(width/2, height/2);
+  translate(p5.Vector.fromAngle(millis() / 600, 80));
+  word4 = text(j, width/10, height/10);
+  pop()
+
+  push()
+  let word5;
+  fill(255);
+  textAlign(CENTER);
+  translate(width/2, height/2);
+  translate(p5.Vector.fromAngle(millis() / 800, 80));
+  word5 = text(d, width/10, height/10);
+  pop()
+
+
+
+ 
+}
+
+
 
 
